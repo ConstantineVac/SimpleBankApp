1103506c909bf225a4b36183f8654712ed41a1d4
ConstantineVac <crystalhakers@hotmail.com>
2023-06-13 20:59:45 +0300

    Initial Commit

---
+++ src/gr/aueb/cf/model/OverDraftAccount.java
@@ -0,0 +1,28 @@
+package gr.aueb.cf.model;
+
+import gr.aueb.cf.exceptions.InsufficientBalanceException;
+import gr.aueb.cf.exceptions.NegativeAmountException;
+import gr.aueb.cf.exceptions.SsnNotValidException;
+
+public class OverDraftAccount extends Account {
+
+    public OverDraftAccount () {}
+
+    public OverDraftAccount(User holder, String iban, double balance) {
+        super(holder, iban, balance);
+    }
+
+    @Override
+    public void withdraw(double amount, String ssn)
+            throws SsnNotValidException, NegativeAmountException {
+        try {
+            if (amount < 0) {throw new NegativeAmountException(amount);}
+            if (!isSsnValid(ssn)) {throw new SsnNotValidException(ssn);}                            // Inherited by Account.
+
+            setBalance(getBalance() - amount);                                                      // It's private so we use set and get.
+        } catch (SsnNotValidException | NegativeAmountException e) {
+            System.out.println("Error: withdrawal");
+            throw e;
+        }
+    }
+}
